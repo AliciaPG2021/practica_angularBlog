f24065263d6bb9d75e7a3026ec7a62cdf95fde48
Alicia <aliciapradag@gmail.com>
2021-05-22 09:37:16 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogComponent } from './component/blog/blog.component';
+import { FormularioComponent } from './component/formulario/formulario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/blog' },
+  { path: 'blog', component: BlogComponent },
+  { path: 'new', component: FormularioComponent },
+  { path: '**', redirectTo: '/blog' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
